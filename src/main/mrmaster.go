e9c8c8a97a7d51c56afde6a3fb8b4ecb5d48936d
Debashish Deka <dekadebashish3@gmail.com>
2020-05-01 15:14:48 +0530

    temp

--- src/main/mrmaster.go
+++ src/main/mrmaster.go
@@ -9,10 +9,13 @@ package main
 // Please do not change this file.
 //
 
-import "../mr"
-import "time"
-import "os"
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	mr "github.com/ddeka0/mrgo/src/mr"
+)
 
 func main() {
 	if len(os.Args) < 2 {
--- src/main/mrworker.go
+++ src/main/mrworker.go
@@ -10,11 +10,14 @@ package main
 // Please do not change this file.
 //
 
-import "../mr"
-import "plugin"
-import "os"
-import "fmt"
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"plugin"
+
+	mr "github.com/ddeka0/mrgo/src/mr"
+)
 
 func main() {
 	if len(os.Args) != 2 {
